1f789004e43534386db53af81c5691136398027d
benny_neiman <bn161299@gmail.com>
2026-02-23 17:19:28 +0200

    logic

--- src/gridLayout.jsx
+++ src/gridLayout.jsx
@@ -2,7 +2,9 @@ import React, { useState, useRef } from "react";
 
 export default function GridLayout() {
   const randomRef = useRef(Math.floor(Math.random() * 100) + 1);
+  const colorRef = useRef(getRandomColor()).current;
   console.log(randomRef.current);
+  console.log(colorRef);
 
   const [counter, setConter] = useState(0);
   const [msg, setmsg] = useState();
@@ -10,15 +12,21 @@ export default function GridLayout() {
     setTimeout(() => {
       window.location.reload();
     }, 3500);
-
+  function getRandomColor() {
+    var letters = "0123456789ABCDEF";
+    var color = "#";
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
   const hendleClick = (e) => {
     if (e.target.id === "true") {
       setConter((prevCount) => prevCount + 1);
 
       setmsg(`You found the number after ${counter} tries 🤓🤓🤓🤓`);
-      e.target.classList.add("bg-red-600", "hover:bg-red-500");
-      const lodeing = document.getElementById("lodeing");
-      lodeing.classList.remove("hidden");
+      e.target.style.backgroundColor = colorRef;
+    
       realued();
 
       e.target.textContent = "V";
@@ -26,6 +34,7 @@ export default function GridLayout() {
       if (e.target.textContent != "X") {
         setmsg("Keep Searching!");
         e.target.textContent = "X";
+        e.target.classList.add("bg-stone-700")
         setConter((prevCount) => prevCount + 1);
       }
     }
@@ -46,11 +55,14 @@ export default function GridLayout() {
 
   return (
     <div className="bg-blue-500 grid grid-cols-10 grid-rows-10 gap-.5 pl-10 pr-10">
-      <div className="col-span-10 text-center font-sans text-4xl">
-        Find thes <span className="text-purple-700"> Secret Color 🧐!</span>
+      <div className="col-span-10 text-center font-sans text-4xl ">
+        Find thes <span className="text-purple-700 "> Secret Color 🧐!</span>
       </div>
 
-      <div className="col-start-5 col-end-7 row-span-2 bg-red-700 text-center text-white pt-5 mt-1 text-2xl">
+      <div
+        style={{ backgroundColor: colorRef }}
+        className={`col-start-5 col-end-7 row-span-2 text-center text-white pt-5 mt-1 text-2xl motion-preset-confetti `}
+      >
         {" "}
         secret color!
       </div>
@@ -60,7 +72,7 @@ export default function GridLayout() {
         arr.map((element, j) => (
           <div
             onClick={hendleClick}
-            className="col-span-1 bg-gray-700 hover:bg-gray-500 border h-10 text-white text-center  cursor-pointer "
+            className="col-span-1 bg-gray-500 hover:bg-gray-300 border h-10 text-white text-center  cursor-pointer "
             id={element == randomRef.current ? "true" : `${element}`}
           >
             {" "}
@@ -69,33 +81,15 @@ export default function GridLayout() {
         )),
       )}
 
-      <div className="col-span-10 text-center text-white text-4xl border-b-4 border-gray">
+      <div className="col-span-10 text-center text-white text-4xl border-b-4 border-gray ">
         clicks: {counter}
       </div>
-      
+
       <div className="col-span-10 row-span-2 justify-center text-yellow-300 text-4xl font-mono text-center">
         {msg}
-        <div id="lodeing" className="hidden" role="status">
-          <svg
-            aria-hidden="true"
-            className="inline w-8 h-8 text-neutral-tertiary animate-spin fill-danger"
-            viewBox="0 0 100 101"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-              fill="currentColor"
-            />
-            <path
-              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-              fill="currentFill"
-            />
-          </svg>
-          <span className="sr-only">Loading...</span>
-        </div>
-        
+    
       </div>
+      <div className="text-center col-span-10 ">🤩🤩🤩🤩🤩🤩🤩</div>
     </div>
   );
 }
